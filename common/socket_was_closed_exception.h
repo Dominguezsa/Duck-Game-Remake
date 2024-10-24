6a0b31cf4dfeed863a4a0106e0ec36ef9deb0236
francordgz <frodriguez@fi.uba.ar>
2024-10-24 18:59:19 -0300

    Add: Agrego clase Protocolo abstracta

---
+++ common/socket_was_closed_exception.h
@@ -0,0 +1,11 @@
+#ifndef SOCKET_WAS_CLOSED_EXCEPTION
+#define SOCKET_WAS_CLOSED_EXCEPTION
+
+#include "common_liberror.h"
+
+class SocketWasCLosedException : public LibError {
+    // Hereda el constructor de la clase LibError
+    using LibError::LibError;
+};
+
+#endif // SOCKET_WAS_CLOSED_EXCEPTION
