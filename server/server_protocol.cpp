230178f5d3252df6d5803f7817557bccb6dfd85b
francordgz <frodriguez@fi.uba.ar>
2024-11-09 19:24:44 -0300

    Feat: Métodos para el envio de datos al cliente tras unirse/crear una partida.

--- server/server_protocol.cpp
+++ server/server_protocol.cpp
@@ -47,3 +47,50 @@ void ServerProtocol::send_msg(void* msg) {
     int int_msg = *static_cast<int*>(msg);
     std::cout << "Sending message: " << int_msg << std::endl;
 }
+
+// --------- Pre-match messages (lobby) -------
+
+void ServerProtocol::send_game_map_list(std::list<std::string>& map_names) {
+    uint16_t map_count = map_names.size();
+    map_count = htons(map_count);
+    send_data(&map_count, sizeof(uint16_t));
+    
+    for (std::string map_name : map_names) {
+        send_string(map_name);
+    }
+}
+
+void ServerProtocol::recv_match_info(std::string& map_name, std::string& match_name,
+                                     uint8_t& number_of_players) {
+    recv_string(map_name);
+    recv_string(match_name);
+    recv_uint_8(number_of_players);
+}
+
+void ServerProtocol::send_confirmation(bool success) {
+    send_data(&success, sizeof(uint8_t));
+}
+
+void ServerProtocol::send_match_list(std::list<std::string>& match_names) {
+    uint16_t match_count = match_names.size();
+    match_count = htons(match_count);
+    send_data(&match_count, sizeof(uint16_t));
+    
+    for (std::string match_name : match_names) {
+        send_string(match_name);
+    }
+}
+
+void ServerProtocol::recv_action(char& action) { 
+    uint8_t buf;
+    recv_uint_8(buf);
+    action = static_cast<char>(buf); 
+}
+
+void ServerProtocol::recv_match_name(std::string& match_name) {
+    recv_string(match_name);
+}
+
+void ServerProtocol::recv_player_name(std::string& player_name) {
+    recv_string(player_name);
+}
