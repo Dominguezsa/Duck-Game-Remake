fdff3b6db47165c04c8dd340a3c918dda6aa7042
Dominguezsa <sadominguez@fi.uba.ar>
2024-11-16 17:28:07 -0300

    mini change

--- client/ScreenRenderer.cpp
+++ client/ScreenRenderer.cpp
@@ -57,16 +57,17 @@ void ScreenRenderer::copyDucks(const std::vector<Duck>& ducks, const int it) {
 void ScreenRenderer::copyGuns(const std::vector<Duck>& ducks) {
     try {
         for (int i = 0; i < (int)ducks.size(); i++) {
+            SDL2pp::Texture& weapon_texture = *resourceManager.getTexture(ducks[i].weapon.name);
             if (ducks[i].looking == 0) {
                 renderer.Copy(
-                        *resourceManager.getTexture(ducks[i].weapon.name), SDL2pp::NullOpt,
+                        weapon_texture, SDL2pp::NullOpt,
                         SDL2pp::Rect(ducks[i].position.x + (DUCK_WIDTH * DUCK_SCALE) / 2.9,
                                      ducks[i].position.y + (DUCK_HEIGTH * DUCK_SCALE) / 2.2,
                                      DUCK_ARM_WIDTH * DUCK_SCALE, DUCK_ARM_HEIGTH * DUCK_SCALE),
                         0.0, SDL2pp::NullOpt, SDL_FLIP_HORIZONTAL);
             } else {
                 renderer.Copy(
-                        *resourceManager.getTexture(ducks[i].weapon.name), SDL2pp::NullOpt,
+                        weapon_texture, SDL2pp::NullOpt,
                         SDL2pp::Rect(ducks[i].position.x + (DUCK_ARM_WIDTH * DUCK_SCALE) / 3.5,
                                      ducks[i].position.y + (DUCK_HEIGTH * DUCK_SCALE) / 2.2,
                                      DUCK_ARM_WIDTH * DUCK_SCALE, DUCK_ARM_HEIGTH * DUCK_SCALE));
