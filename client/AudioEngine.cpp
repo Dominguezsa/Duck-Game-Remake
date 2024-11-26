b8051adf0a18ab8ac414fc835239f44a01c8efd2
Dominguezsa <sadominguez@fi.uba.ar>
2024-11-26 10:36:59 -0300

    doesnt play audio so it doesnt break

--- client/AudioEngine.cpp
+++ client/AudioEngine.cpp
@@ -5,11 +5,11 @@ AudioEngine::AudioEngine(std::vector<DuckState>& ducks, SDL2pp::Mixer& mixer,
         ducks(ducks), mixer(mixer), resourceManager(resourceManager) {}
 
 void AudioEngine::playAudio() {
-    for (const auto& duck: ducks) {
+    /*for (const auto& duck: ducks) {
         if (duck.is_shooting) {
             mixer.PlayChannel(-1, *resourceManager.getSFX("boom11"), -1);
         } else {
             mixer.FadeOutChannel(-1, 200);
         }
-    }
+    }*/
 }
