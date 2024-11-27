ff7905be96c8ed602eae7d66f865a11452e81a08
ValFran <francovalfre999@gmail.com>
2024-11-26 22:30:47 -0300

    fix: now the audioEngine will play a sound every time any duck shoots

--- client/AudioEngine.cpp
+++ client/AudioEngine.cpp
@@ -5,11 +5,13 @@ AudioEngine::AudioEngine(std::vector<DuckState>& ducks, SDL2pp::Mixer& mixer,
         ducks(ducks), mixer(mixer), resourceManager(resourceManager) {}
 
 void AudioEngine::playAudio() {
-    /*for (const auto& duck: ducks) {
+    for (const auto& duck: ducks) {
         if (duck.is_shooting) {
-            mixer.PlayChannel(-1, *resourceManager.getSFX("boom11"), -1);
+            if (mixer.IsChannelPlaying(1) == 0) {
+                mixer.PlayChannel(1, *resourceManager.getSFX("boom11"), -1);
+            }
         } else {
-            mixer.FadeOutChannel(-1, 200);
+            mixer.FadeOutChannel(1, 200);
         }
-    }*/
+    }
 }
