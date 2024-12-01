8f3709d531bd3419c8be541c1b1dfd2852924b93
Dominguezsa <sadominguez@fi.uba.ar>
2024-12-01 13:07:50 +0000

    añado enum para imagenes

---
+++ editor/image_type.h
@@ -0,0 +1,34 @@
+#ifndef IMAGE_TYPE
+#define IMAGE_TYPE
+
+enum WeaponType {
+    left_simple_platform,
+    middle_simple_platform,
+    right_simple_platform,
+    left_up_big_platform,
+    middle_up_big_platform,
+    right_up_big_platform,
+    left_middle_big_platform,
+    middle_middle_big_platform,
+    right_middle_big_platform,
+    left_down_big_platform,
+    middle_down_big_platform,
+    right_down_big_platform,
+    respawn_for_player,
+    mistery_box, //por el momento no sirve de nada
+    coin,
+    magnum,
+    duelGun,
+    revolver,
+    ak47,
+    shotgun,
+    sniper,
+    lasergun,
+    sniper,
+    banana,
+    grenade,
+    helmet,
+    shield,
+};
+
+#endif  // IMAGE_TYPE
