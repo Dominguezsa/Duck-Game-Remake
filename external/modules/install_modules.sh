bea5cafee8af998c2954dce338d72e67cdf269eb
francordgz <frodriguez@fi.uba.ar>
2024-12-18 07:39:56 -0300

    Installing the project on Ubuntu 22.04 and 24.04

--- external/modules/install_modules.sh
+++ external/modules/install_modules.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Required by Ubuntu 22.04
 sudo apt-get install libmodplug-dev
+
+# Required by Ubuntu 24.04
+sudo apt-get install libopusfile-dev libfreetype6-dev libjpeg-dev libflac-dev libfluidsynth.dev
 
 echo "Installing SDL2 and additional libraries..."
 cd SDL2_image-2.6.3  # .../external/modules/SDL2_image-2.6.3
